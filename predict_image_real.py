6a05dc7b09ba13a4499eccacccc18acc86e95f23
Aditya venkat <5437668adityavenkat@gmail.com>
2026-08-14 13:11:51 +0530

    Add patch_size argument to argument parser

--- predict_image_real.py
+++ predict_image_real.py
@@ -227,5 +227,6 @@ if __name__ == "__main__":
     parser.add_argument("--layer2", type=int, default=11)
     parser.add_argument("--clip_crop_r1", type=float, default=0.6)
     parser.add_argument("--clip_crop_r2", type=float, default=0.9)
+    parser.add_argument("--patch_size", type=int, default=14)
     args = parser.parse_args()
     main(args)
