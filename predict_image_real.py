8f95903393ca8a7410bef0249f1081d7b5fbc1ec
Aditya venkat <5437668adityavenkat@gmail.com>
2026-08-17 12:42:46 +0530

    Fix device assignment for attention images in prediction

--- predict_image_real.py
+++ predict_image_real.py
@@ -44,6 +44,8 @@ def run_custom_loader(path, model_dino, processor_dino, processor, args):
     augmented_imgs = [processor_dino(img, return_tensors="pt")["pixel_values"].squeeze(0)]
     augmented_imgs.extend(processor(img) for _ in range(1))
     attention_imgs_dino = processor_dino(img, return_tensors="pt")
+    dino_device = next(model_dino.parameters()).device
+    attention_imgs_dino = {k: v.to(dino_device) for k, v in attention_imgs_dino.items()}
     with torch.no_grad():
         image_attention_mh = model_dino(**attention_imgs_dino, output_attentions=True)
         image_attention_mh = image_attention_mh.attentions
